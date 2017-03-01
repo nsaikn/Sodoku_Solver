bafbf44476bd6ddcc407d4ef0b304a4545b49783
IBMHACK <schalasani1@myseneca.ca>
2017-03-01 02:39:40 -0500

    completed Version 1 of sudoku solver. functional but not the most efficient

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -5,28 +5,44 @@
 
 ######################### FUNCTIONS #########################################
 def Compare(a, b): # a is y position and b is x position 
+	rowOptions = []
+	columnOptions = []
+	boxOptions = []
 	#compares column
 	for y in range(0,9):
 		if not isinstance(sudoku[(y,b)], list): #true if box is not blank
 			if sudoku[(y,b)] in sudoku[(a,b)]:
 				sudoku[(a,b)].remove(sudoku[(y,b)])
-				#need to make a check to see if it is sole element left
+		elif y != a:
+			columnOptions += sudoku[(y,b)]
+		
 	# Compares row
 	for x in range(0,9):
 		if not isinstance(sudoku[(a,x)], list): #true if box is not blank
 			if sudoku[(a,x)] in sudoku[(a,b)]:
 				sudoku[(a,b)].remove(sudoku[(a,x)])
-
+		elif x != b:
+			rowOptions += sudoku[(a,x)]
 
 	for y in range(int(a/3)*3, int(a/3)*3+3):
 		for x in range(int(b/3)*3, int(b/3)*3+3):
 			if not isinstance(sudoku[(y,x)], list):
 				if sudoku[(y,x)] in sudoku[(a,b)]:
 					sudoku[(a,b)].remove(sudoku[(y,x)])
+			elif y != a or x != b:
+				boxOptions += sudoku[(y,x)]
+
 	if len(sudoku[(a,b)]) == 1:
 		sudoku[(a,b)] =  sudoku[(a,b)][0]
 		return True
 	else:
+		# columnOptions = set(columnOptions)
+		# rowOptions = set(rowOptions)
+		# boxOptions = set(boxOptions)
+		for each in sudoku[(a,b)]:
+			if not each in columnOptions or not each in rowOptions or not each in boxOptions:
+				sudoku[(a,b)] = each
+				return True
 		return False
 
 def Display():
@@ -61,16 +77,20 @@ for y in range (0,9):
 
 
 notSolved = True
+Display()
+
 while(notSolved):
 	notSolved = False
-	Display()
-	input()
 	for y in range(0,9):
 		for x in range(0,9):
+
 			if isinstance(sudoku[(y,x)], list):
 				if not Compare(y,x):
 					notSolved = True
+	
 
+Display()	
+input()	
 
 
 
